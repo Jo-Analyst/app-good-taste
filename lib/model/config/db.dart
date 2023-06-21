63f7d1c0f0e2b777652d3a7de34c99858dea23a0
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-06-21 11:45:45 -0300

    criação da configuração da da base de dados no sqlite

---
+++ lib/model/config/db.dart
@@ -0,0 +1,27 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DB {
+  static Future<sql.Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(
+      path.join(dbPath, "icecream.db"),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE juices(id integer PRIMARY KEY, name TEXT, brand TEXT, price REAL)",
+        );
+      },
+    );
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DB.database();
+    await db.insert(table, data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DB.database();
+    return db.query(table);
+  }
+}
