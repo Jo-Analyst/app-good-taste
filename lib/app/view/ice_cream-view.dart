487e375adcd3fceb14761ad823055909f6257c6c
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-06-23 21:10:50 -0300

    colocano icones

--- lib/app/view/ice_cream-view.dart
+++ lib/app/view/ice_cream-view.dart
@@ -7,14 +7,66 @@ class IceCreamView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text("Ice Cream"),
-        backgroundColor: Colors.pink.shade200,
-        // centerTitle: true,
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: AppBar(
+          title: const Text("ICEM CREAM - JV"),
+          // title: const Align(
+          //   alignment: Alignment.topLeft,
+          //   child: Text("ICEM CREAM - JV"),
+          // ),
+          backgroundColor: Colors.pink.shade200,
+        ),
       ),
       body: const Center(
         child: Text("Ice Cream"),
+      ),
+      extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.pink.shade200,
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: Colors.pink.shade200,
+        child: IconTheme(
+          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.home,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.save_alt_sharp,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.price_check_sharp,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.list_alt_rounded,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
