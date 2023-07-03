c4bccc7a8136b85fc505d52408a9edd38a9d5448
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-07-03 11:45:26 -0300

    começo de colocar os sabores

--- lib/app/page/product_form.dart
+++ lib/app/page/product_form.dart
@@ -9,33 +9,89 @@ class ProductFormPage extends StatefulWidget {
 
 class _ProductFormPageState extends State<ProductFormPage> {
   final _key = GlobalKey<FormState>();
+  List<String> flavors = [
+    "Morango",
+    "Abacaxi",
+    "Maracujá",
+    "Uva",
+    "Baunilha com limão",
+    "Iorgute de morango",
+    "Iorgute de maracujá",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Meu Produto"),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.check,
+                size: 35,
+              ))
+        ],
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-        child: Form(
-          key: _key,
-          child: Column(
-            children: [
-              Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: "Nome"),
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 15,
+            ),
+            child: Column(
+              children: [
+                Form(
+                  key: _key,
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: "Nome"),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          TextFormField(
+                            keyboardType: const TextInputType.numberWithOptions(
+                              decimal: true,
+                            ),
+                            textInputAction: TextInputAction.next,
+                            decoration:
+                                const InputDecoration(labelText: "Preço"),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  TextFormField(
-                    keyboardType: const TextInputType.numberWithOptions(
-                      decimal: true,
-                    ),
-                    decoration: const InputDecoration(labelText: "Preço"),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Sabores: ",
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.add_circle_outline,
+                          color: Theme.of(context).primaryColor,
+                          size: 40,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
